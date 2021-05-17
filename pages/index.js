4f50134262d87d38967b851dfd76c246f0446391
chrischongyj <39231238+chrischongyj@users.noreply.github.com>
2021-05-17 17:54:26 +0800

    Test commit

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home({ isConnected }) {
         </h1>
 
         {isConnected ? (
-          <h2 className="subtitle">You are connected to MongoDB</h2>
+          <h2 className="subtitle">You are connected to MongoDB bitch</h2>
         ) : (
           <h2 className="subtitle">
             You are NOT connected to MongoDB. Check the <code>README.md</code>{' '}
